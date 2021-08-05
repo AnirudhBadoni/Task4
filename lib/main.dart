eae8dea7132e200866d56c6b4bf03792f93749f9
Your Name <you@example.com>
2021-08-04 23:32:14 -0700

    new project idea

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'user1.dart';
-import 'user2.dart';
-import 'user3.dart';
-import 'user4.dart';
-import 'user5.dart';
+
 import 'secondScreen.dart';
 
 void main() {
@@ -20,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigations',
+      title: 'Navigation',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
@@ -38,116 +34,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String email = '';
-  String _launchUrl = 'https://mail.google.com/mail/u/0/#inbox';
+  String _launchUrl = 'https://chat.whatsapp.com/ES9xu2avcYT9RH4yLLN4Eu';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Get In Contact',
-        ),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 10.0),
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/admin.jfif'),
-              radius: 25.0,
-            ),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text('admin'),
-              accountEmail: Text('admin@gmail.com'),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/admin.jfif'),
-              ),
-            ),
-            ListTile(
-              title: Text('user 1'),
-              onTap: () {
-                Navigator.of(context).pop;
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => User1()));
-              },
-            ),
-            Divider(
-              color: Colors.black12,
-              height: 10.0,
-            ),
-            ListTile(
-              title: Text('user 2'),
-              onTap: () {
-                Navigator.of(context).pop;
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => User2()));
-              },
-            ),
-            Divider(
-              color: Colors.black12,
-              height: 10.0,
-            ),
-            ListTile(
-              title: Text('user 3'),
-              onTap: () {
-                Navigator.of(context).pop;
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => User3()));
-              },
-            ),
-            Divider(
-              color: Colors.black12,
-              height: 10.0,
-            ),
-            ListTile(
-              title: Text('user 4'),
-              onTap: () {
-                Navigator.of(context).pop;
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => User4()));
-              },
-            ),
-            Divider(
-              color: Colors.black12,
-              height: 10.0,
-            ),
-            ListTile(
-              title: Text('user 5'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => User5()));
-              },
-            ),
-          ],
+          'Get In Contact for private group',
         ),
       ),
       body: Column(
         children: <Widget>[
-          Expanded(
-            child: Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(0.0),
-              color: Colors.blueAccent,
-              child: Image(
-                image: NetworkImage(
-                    'https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'),
-              ),
-            ),
-          ),
           Expanded(
             child: Container(
                 alignment: Alignment.center,
@@ -162,8 +60,11 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     RaisedButton(
-                      onPressed: () {launch('https://mail.google.com/mail/u/0/#inbox');},
-                      child: Text('Follow Me On Facebook'),
+                      onPressed: () {
+                        launch(
+                            'https://chat.whatsapp.com/ES9xu2avcYT9RH4yLLN4Eu');
+                      },
+                      child: Text('Follow Me On gmail'),
                     ),
                   ],
                 )),
@@ -182,7 +83,10 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     RaisedButton(
-                      onPressed: () {launch('https://www.facebook.com/friends');},
+                      onPressed: () {
+                        launch(
+                            'https://chat.whatsapp.com/ES9xu2avcYT9RH4yLLN4Eu');
+                      },
                       child: Text('Follow Me On Facebook'),
                     ),
                   ],
@@ -202,7 +106,10 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     RaisedButton(
-                      onPressed: () {launch('https://www.linkedin.com/feed/');},
+                      onPressed: () {
+                        launch(
+                            'https://chat.whatsapp.com/ES9xu2avcYT9RH4yLLN4Eu');
+                      },
                       child: Text('follow me on linkedin'),
                     ),
                   ],
@@ -210,73 +117,28 @@ class _HomePageState extends State<HomePage> {
           ),
           Expanded(
             child: Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.all(50.0),
-                color: Colors.blueAccent,
-                child: Row(
-                  children: [
-                    Container(
-                      child: Icon(
-                        FontAwesomeIcons.github,
-                        size: 50.0,
-                      ),
-                    ),
-                    RaisedButton(
-                      onPressed: () {launch('https://github.com/');},
-                      child: Text('follow me on github'),
-                    ),
-                  ],
-                )),
-          ),
-          Expanded(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(50.0),
+              color: Colors.blueAccent,
               child: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  alignment: Alignment.center,
-                  color: Colors.blueAccent,
-                  child: TextField(
-                    cursorColor: Colors.black,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(width: 10),
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(30.0),
-                        ),
-                      ),
-                      hintText: 'add comments',
+                children: [
+                  Container(
+                    child: Icon(
+                      FontAwesomeIcons.github,
+                      size: 50.0,
                     ),
                   ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  alignment: Alignment.center,
-                  //padding: EdgeInsets.all(10.0),
-                  color: Colors.blueAccent,
-                  child: FlatButton(
+                  RaisedButton(
                     onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SecondScreen(),
-                        ),
-                      );
+                      launch(
+                          'https://chat.whatsapp.com/ES9xu2avcYT9RH4yLLN4Eu');
                     },
-                    //padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
-                    color: Colors.red,
-                    textColor: Colors.white,
-                    child: Text(
-                      'Submit',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
+                    child: Text('follow me on github'),
                   ),
-                ),
-              )
-            ],
-          )),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
